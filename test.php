da5a5f06fac23ba551807febdfc9a4ae2849593e
Karoline Barreto <karolinedbarreto@gmail.com>
2024-07-20 11:42:22 -0300

    test(DiscountCalculator): make test

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 
 use Src\Budget;
+use Src\DiscountCalculator;
 use Src\Tax\Icms;
 use Src\Tax\Iss;
 use Src\TaxCalculator;
@@ -14,3 +15,10 @@ $budget->value = 100;
 
 echo 'icms value: ' . $calculator->calculate($budget, new Icms());
 echo ' | iss value: ' . $calculator->calculate($budget, new Iss());
+
+$discountCalculator = new DiscountCalculator();
+
+$budget->value = 600;
+$budget->itensQuantity = 5;
+
+echo '| discount calculator: ' . $discountCalculator->calculate($budget);
